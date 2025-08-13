3e8e893b904ca1b893ecedf1b3c5e4c6f4416e07
Darine Ben Belkhir <darinebelkhir2019@gmail.com>
2025-08-13 18:32:58 +0100

    initializing pizzaplatforme

---
+++ src/main/java/com/darine/pizzaplatforme/pizzaplatforme/PizzaPlatformeApplication.java
@@ -0,0 +1,13 @@
+package com.darine.pizzaplatforme.pizzaplatforme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaPlatformeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaPlatformeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/darine/pizzaplatforme/pizzaplatforme/PizzaPlatformeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darine.pizzaplatforme.pizzaplatforme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaPlatformeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
